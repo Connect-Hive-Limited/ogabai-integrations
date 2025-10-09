a0205c4063e1cd1c115f704e508cdc7d8e15a2db
Agu Chijioke <chokey2nv@gmail.com>
2025-10-09 14:05:09 +0100

    updated package configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
   dts: true,
   format: ["cjs", "esm"],
   outDir: "dist",
-  clean: true,
-  outExtension: ({ format }) => ({
-    js: format === "cjs" ? ".cjs.js" : ".esm.js",
-  }),
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs.js" : ".esm.js"
+    };
+  },
+  target: "es2018",
+  platform: "neutral",
+  treeshake: true,
+  minify: false
 });
