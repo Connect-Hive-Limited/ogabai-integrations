3bb0738f76214a48aeccff61fd311c9d9b89705e
Agu Chijioke <chokey2nv@gmail.com>
2026-02-13 12:59:08 +0100

    add subscriptoin endpoints

--- src/services/subscription/index.ts
+++ src/services/subscription/index.ts
@@ -2,3 +2,4 @@ export * from "./paystack.service";
 export * from "./types";
 export * from "./subscription-plan-feature.service"
 export * from "./subscription-plan.service"
+export * from "./subscription.service"
